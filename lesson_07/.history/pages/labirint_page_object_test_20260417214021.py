1e3f65a62ae9ccf86ca39ebed6831dc858e1d006
tanksmagistr-jpg <tanksmagistr@gmail.com>
2026-04-21 21:41:16 +0500

    lesson7: удалил лишнее оставил только файлы с домашки

--- lesson_07/.history/pages/labirint_page_object_test_20260417214021.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-cookie = {"name": "cookie_policy", "value": "1"}
-
-browser = webdriver.Chrome()
-
-
-def test_cart_counter():
